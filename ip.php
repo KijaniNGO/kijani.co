e7cae84f165cbc7bc6957f9cf4d47684a56b5778
MrLoh <flojoto@hotmail.com>
2014-06-07 19:38:09 +0200

    small fixes

--- ip.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$locale = shell_exec('geoip-lookup '.$_SERVER['REMOTE_ADDR']);
-	if( $locale == "DE\n" || $locale == "AT\n" || $locale == "CH\n") {
-		header('Location: http://kijani.co/donate');
-	} else{
-		header('Location: http://kijani.co/newsletter');
-	};
-?>
