db347020235da3ef24a7e11ee2c35fe2442ed66b
MrLoh <flojoto@hotmail.com>
2014-06-10 17:19:19 +0200

    changed language select by ip to language select by browser language

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-	$locale = shell_exec('geoip-lookup '.$_SERVER['REMOTE_ADDR']);
-	if( $locale == "DE\n" || $locale == "AT\n" || $locale == "CH\n") {
-		header('Location: de');
-	} else{
-		header('Location: en');
-	};
+// $locale = shell_exec('geoip-lookup '.$_SERVER['REMOTE_ADDR']);
+// if( $locale == "DE\n" || $locale == "AT\n" || $locale == "CH\n") {
+// 	header('Location: de');
+// } else{
+// 	header('Location: en');
+// };
+
+$locale = 'en';
+$accept_langs = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+foreach ( $accept_langs as $lang ) {
+	$locale = preg_match('/^de/', $lang) ? 'de' : $locale;
+}
+header("Location: $locale");
 ?>
